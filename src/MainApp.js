958c046a8393776f531ffb51aacfe52775e310bb
John Leonardo <jdleonardo5@icloud.com>
2019-07-10 16:08:21 -0700

    working search filtering

--- src/MainApp.js
+++ src/MainApp.js
@@ -3,9 +3,27 @@ import './App.css';
 import emojiJson from './emoji.json'
 import { Container, Row, Col } from 'reactstrap';
 
-var currentEmojis = emojiJson.slice(0, 100);
+var currentEmojis = emojiJson.slice(0, 200);
 
 class MainApp extends React.Component {
+
+  //helper method to get the char of the current emoji in array
+  getChar(index) {
+    if (currentEmojis[index]) {
+      return currentEmojis[index].char;
+    } else {
+      return -1;
+    }
+  }
+
+  //helper method to get the char of the current emoji name in array
+  getName(index) {
+    if (currentEmojis[index]) {
+        return currentEmojis[index].name;
+    } else {
+        return -1;
+    }
+  }
 
   renderEmojiList() {
     //numbers by groups of fours
@@ -15,56 +33,62 @@ class MainApp extends React.Component {
       nums.push(i * 4);
     }
 
-    return (
-      <Container className="Main-container">
-        {
-          nums.map(num =>
-            <Row>
-              <Col style={{display: "inline-block", margin: "10px"}} key={currentEmojis[num].char}>
-                <div className="Card">
-                  <p className="Emoji-text">
-                    {currentEmojis[num].char}
-                  </p>
-                  <p className="Name-text">
-                    {currentEmojis[num].name}
-                  </p>
-                </div>
-              </Col>
-              <Col style={{display: "inline-block", margin: "10px"}} key={currentEmojis[num+1].char}>
-                <div className="Card">
-                  <p className="Emoji-text">
-                    {currentEmojis[num+1].char}
-                  </p>
-                  <p className="Name-text">
-                    {currentEmojis[num+1].name}
-                  </p>
-                </div>
-              </Col>
-              <Col style={{display: "inline-block", margin: "10px"}} key={currentEmojis[num+2].char}>
-                <div className="Card">
-                  <p className="Emoji-text">
-                    {currentEmojis[num+2].char}
-                  </p>
-                  <p className="Name-text">
-                    {currentEmojis[num+2].name}
-                  </p>
-                </div>
-              </Col>
-              <Col style={{display: "inline-block", margin: "10px"}} key={currentEmojis[num].char}>
-                <div className="Card">
-                  <p className="Emoji-text">
-                    {currentEmojis[num+3].char}
-                  </p>
-                  <p className="Name-text">
-                    {currentEmojis[num+3].name}
-                  </p>
-                </div>
-              </Col>
-            </Row>
-          )
-        }
-      </Container>
-    );
+    if (currentEmojis.length == 0) {
+      return (
+        <p style={{color: "#153366", "font-weight": "800", "font-size": "2.85em"}}>Sorry, no results here. 😢😢😢</p>
+      );
+    } else {
+      return (
+        <Container className="Main-container">
+          {
+            nums.map(num =>
+              <Row>
+                <Col style={{display: "inline-block", margin: "10px", visibility: (this.getChar(num) == -1) ? "hidden": "visible"}} key={this.getChar(num)}>
+                  <div className="Card">
+                    <p className="Emoji-text">
+                      {this.getChar(num)}
+                    </p>
+                    <p className="Name-text">
+                      {this.getName(num)}
+                    </p>
+                  </div>
+                </Col>
+                <Col style={{display: "inline-block", margin: "10px", visibility: (this.getChar(num+1) == -1) ? "hidden": "visible"}} key={this.getChar(num+1)}>
+                  <div className="Card">
+                    <p className="Emoji-text">
+                      {this.getChar(num+1)}
+                    </p>
+                    <p className="Name-text">
+                      {this.getName(num+1)}
+                    </p>
+                  </div>
+                </Col>
+                <Col style={{display: "inline-block", margin: "10px", visibility: (this.getChar(num+2) == -1) ? "hidden": "visible"}} key={this.getChar(num+2)}>
+                  <div className="Card">
+                    <p className="Emoji-text">
+                      {this.getChar(num+2)}
+                    </p>
+                    <p className="Name-text">
+                      {this.getName(num+2)}
+                    </p>
+                  </div>
+                </Col>
+                <Col style={{display: "inline-block", margin: "10px", visibility: (this.getChar(num+3) == -1) ? "hidden": "visible"}} key={this.getChar(num+3)}>
+                  <div className="Card">
+                    <p className="Emoji-text">
+                      {this.getChar(num+3)}
+                    </p>
+                    <p className="Name-text">
+                      {this.getName(num+3)}
+                    </p>
+                  </div>
+                </Col>
+              </Row>
+            )
+          }
+        </Container>
+      );
+    }
   }
 
   constructor(props) {
@@ -75,8 +99,12 @@ class MainApp extends React.Component {
   }
 
   handleChange (e) {
-    console.log(e.target.value);
-    this.setState({textValue: e.target.value});
+    //get current text field value
+    const txt = e.target.value;
+    //filter array based on search query
+    currentEmojis = emojiJson.filter(emoji => emoji.name.includes(txt));
+    //set state to trigger react reload
+    this.setState({textValue: txt});
   }
 
   render () {
