57dba355b667f74a0f9c807220c3d0211499be47
John Leonardo <jdleonardo5@icloud.com>
2019-07-10 15:43:53 -0700

    only load first 100 emojis at beginning

--- src/App.js
+++ src/App.js
@@ -1,76 +1,11 @@
 import React from 'react';
 import './App.css';
 import MainApp from './MainApp'
-import emojiJson from './emoji.json'
-import { Container, Row, Col } from 'reactstrap';
-
-
-function renderEmojiList() {
-  //numbers by groups of fours
-  var nums = [];
-  //create list
-  for (var i = 0; i < emojiJson.length / 4; i++) {
-    nums.push(i * 4);
-  }
-
-  return (
-    <Container className="Main-container">
-      {
-        nums.map(num =>
-          <Row>
-            <Col style={{display: "inline-block", margin: "10px"}} key={emojiJson[num].char}>
-              <div className="Card">
-                <p className="Emoji-text">
-                  {emojiJson[num].char}
-                </p>
-                <p className="Name-text">
-                  {emojiJson[num].name}
-                </p>
-              </div>
-            </Col>
-            <Col style={{display: "inline-block", margin: "10px"}} key={emojiJson[num+1].char}>
-              <div className="Card">
-                <p className="Emoji-text">
-                  {emojiJson[num+1].char}
-                </p>
-                <p className="Name-text">
-                  {emojiJson[num+1].name}
-                </p>
-              </div>
-            </Col>
-            <Col style={{display: "inline-block", margin: "10px"}} key={emojiJson[num+2].char}>
-              <div className="Card">
-                <p className="Emoji-text">
-                  {emojiJson[num+2].char}
-                </p>
-                <p className="Name-text">
-                  {emojiJson[num+2].name}
-                </p>
-              </div>
-            </Col>
-            <Col style={{display: "inline-block", margin: "10px"}} key={emojiJson[num].char}>
-              <div className="Card">
-                <p className="Emoji-text">
-                  {emojiJson[num+3].char}
-                </p>
-                <p className="Name-text">
-                  {emojiJson[num+3].name}
-                </p>
-              </div>
-            </Col>
-          </Row>
-        )
-      }
-    </Container>
-  );
-}
 
 function App() {
-
   return (
     <div className="App">
       <MainApp/>
-      {renderEmojiList()}
     </div>
   );
 }
--- src/MainApp.js
+++ src/MainApp.js
@@ -1,7 +1,71 @@
 import React from 'react';
 import './App.css';
+import emojiJson from './emoji.json'
+import { Container, Row, Col } from 'reactstrap';
+
+var currentEmojis = emojiJson.slice(0, 100);
 
 class MainApp extends React.Component {
+
+  renderEmojiList() {
+    //numbers by groups of fours
+    var nums = [];
+    //create list
+    for (var i = 0; i < currentEmojis.length / 4; i++) {
+      nums.push(i * 4);
+    }
+
+    return (
+      <Container className="Main-container">
+        {
+          nums.map(num =>
+            <Row>
+              <Col style={{display: "inline-block", margin: "10px"}} key={currentEmojis[num].char}>
+                <div className="Card">
+                  <p className="Emoji-text">
+                    {currentEmojis[num].char}
+                  </p>
+                  <p className="Name-text">
+                    {currentEmojis[num].name}
+                  </p>
+                </div>
+              </Col>
+              <Col style={{display: "inline-block", margin: "10px"}} key={currentEmojis[num+1].char}>
+                <div className="Card">
+                  <p className="Emoji-text">
+                    {currentEmojis[num+1].char}
+                  </p>
+                  <p className="Name-text">
+                    {currentEmojis[num+1].name}
+                  </p>
+                </div>
+              </Col>
+              <Col style={{display: "inline-block", margin: "10px"}} key={currentEmojis[num+2].char}>
+                <div className="Card">
+                  <p className="Emoji-text">
+                    {currentEmojis[num+2].char}
+                  </p>
+                  <p className="Name-text">
+                    {currentEmojis[num+2].name}
+                  </p>
+                </div>
+              </Col>
+              <Col style={{display: "inline-block", margin: "10px"}} key={currentEmojis[num].char}>
+                <div className="Card">
+                  <p className="Emoji-text">
+                    {currentEmojis[num+3].char}
+                  </p>
+                  <p className="Name-text">
+                    {currentEmojis[num+3].name}
+                  </p>
+                </div>
+              </Col>
+            </Row>
+          )
+        }
+      </Container>
+    );
+  }
 
   constructor(props) {
     super(props)
@@ -34,6 +98,7 @@ class MainApp extends React.Component {
           onChange={(e) => {this.handleChange(e)}}
           className="Input-bar"
         />
+        {this.renderEmojiList()}
       </div>
     )
   }
